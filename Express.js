3ce8218f3a7a7cce87840915f1f73503d6b5cc51
Berseck09 <151967918+Berseck09@users.noreply.github.com>
2023-12-06 12:52:59 -0300

    Create Express.js

---
+++ Express.js
@@ -0,0 +1,34 @@
+PS C:\Users\Facundo\Documents\api_libros> npm init -y
+Wrote to C:\Users\Facundo\Documents\api_libros\package.json:
+
+{
+  "name": "api_libros",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC"
+
+
+PS C:\Users\Facundo\Documents\api_libros> npm install express
+
+added 62 packages, and audited 63 packages in 14s
+
+11 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS C:\Users\Facundo\Documents\api_libros> npm install joi
+
+added 6 packages, and audited 69 packages in 7s
+
+11 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+
+PS C:\Users\Facundo\Documents\api_libros> node app.js
+                                      "Servidor iniciado en el >>  puerto 3000" 
